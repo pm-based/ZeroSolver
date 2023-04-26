8ca2d9ad2ac570d9d1df17e895cb8741729ed6d2
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2023-04-26 15:47:11 +0200

    .

--- pacsChallenges/2_ZeroSolver1/FunctionRtoR.cpp
+++ pacsChallenges/2_ZeroSolver1/FunctionRtoR.cpp
@@ -1,7 +1,12 @@
 #include "FunctionRtoR.hpp"
+#include <iostream>
 
 void FunctionRtoR::setSovler(std::string s){
     auto it = Solver::solverFactory.find(s);
     solver = it->second(fun);    
     //solver->setFun(fun);
 };
+
+void FunctionRtoR::print() const{
+    std::cout<<"AAAAAA"<<std::endl;
+}
