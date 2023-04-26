71916366ac237ce6e80f48acbbe1e1af8048011c
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2023-04-26 13:35:24 +0200

    Test setsetsolver

--- pacsChallenges/2_ZeroSolver1/main.cpp
+++ pacsChallenges/2_ZeroSolver1/main.cpp
@@ -2,9 +2,14 @@
 #include <cmath>
 #include "Traits.hpp"
 #include "FunctionRtoR.hpp"
+#include "LoadSolverFactory.hpp"
+
 
 int main(){
+    Solver::LoadSolverFactory loadPlugins("PluginListToLoad.txt");
+
     FunctionRtoR ChallengeFunction([](double x){return 0.5-std::exp(std::numbers::pi*x);});
+    ChallengeFunction.setSovler("Secant");
 
     return 0;
 }
