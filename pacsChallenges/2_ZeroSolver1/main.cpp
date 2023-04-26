e81080d47b63e1a152060e0041f293798adc3f34
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2023-04-26 15:29:50 +0200

    .

--- pacsChallenges/2_ZeroSolver1/main.cpp
+++ pacsChallenges/2_ZeroSolver1/main.cpp
@@ -10,7 +10,7 @@ int main(){
     Solver::LoadSolverFactory loadPlugins("PluginListToLoad.txt");
 
     FunctionRtoR ChallengeFunction([](double x){return 0.5-std::exp(std::numbers::pi*x);});
-    ChallengeFunction.setSovler(static_cast<std::string>("Secant"));
+    ChallengeFunction.setSovler("Secant");
 
     return 0;
 }
