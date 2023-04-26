74afe59f2133b5c601d821bc71951c216f108818
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2023-04-26 15:55:30 +0200

    .

--- pacsChallenges/2_ZeroSolver1/FunctionRtoR.hpp
+++ pacsChallenges/2_ZeroSolver1/FunctionRtoR.hpp
@@ -9,7 +9,7 @@ class FunctionRtoR: FunctionTraits{
     public:
     FunctionRtoR(FunctionType _fun): fun(_fun){};
     void setSovler(std::string s);
-    void solve() {sol = solver.solve();}
+    void solve() {sol = solver->solve();}
     void print() const;
 
     private:
