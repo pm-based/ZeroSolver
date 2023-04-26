a3c28767c4d32de275535eff09b04c82e1098521
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2023-04-26 15:40:52 +0200

    .

--- pacsChallenges/2_ZeroSolver1/main.cpp
+++ pacsChallenges/2_ZeroSolver1/main.cpp
@@ -1,5 +1,6 @@
 #include <numbers>
 #include <cmath>
+#include <functional>
 #include "Traits.hpp"
 #include "FunctionRtoR.hpp"
 #include "LoadSolverFactory.hpp"
@@ -9,9 +10,9 @@
 int main(){
     Solver::LoadSolverFactory loadPlugins("PluginListToLoad.txt");
 
-    FunctionRtoR ChallengeFunction([](double x){return 0.5-std::exp(std::numbers::pi*x);});
-    std::string s = "Secant";
-    ChallengeFunction.setSovler(s);
+    FunctionTraits::FunctionType FunctionToSolve = [](double x){return 0.5-std::exp(std::numbers::pi*x);};
+    FunctionRtoR ChallengeFunction(FunctionToSolve);
+    ChallengeFunction.setSovler("Secant");
 
     return 0;
 }
