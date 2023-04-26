81766630a1cd7fc7dcb532c2be7ae2376044fa46
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2023-04-26 13:33:28 +0200

    Write a setsolver for functionrtor class

--- pacsChallenges/2_ZeroSolver1/FunctionRtoR.hpp
+++ pacsChallenges/2_ZeroSolver1/FunctionRtoR.hpp
@@ -2,15 +2,20 @@
 #define FUNCTIONRTOR_HPP
 
 #include "Traits.hpp"
+#include "FactoryTraits.hpp"
+#include <string>
 
 class FunctionRtoR: FunctionTraits{
     public:
     FunctionRtoR(FunctionType _fun): fun(_fun){};
+    void setSovler(std::string const s);
+    void solve() const;
+    void print() const;
 
     private:
     FunctionType fun;
     RootType sol;
-    
+    Solver::SolverType solver;
 };
 
 #endif //FUNCTIONRTOR_HPP
