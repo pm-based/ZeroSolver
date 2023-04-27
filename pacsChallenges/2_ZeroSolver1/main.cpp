0e8c260552364f01bc2e7891b50f4414eae78d8c
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2023-04-27 15:54:39 +0200

    try to use Broyden in the main

--- pacsChallenges/2_ZeroSolver1/main.cpp
+++ pacsChallenges/2_ZeroSolver1/main.cpp
@@ -14,7 +14,7 @@ int main(){
 
     FunctionTraits::FunctionType FunctionToSolve = [](double x){return 0.5-std::exp(std::numbers::pi*x);};
     FunctionRtoR ChallengeFunction(FunctionToSolve);
-    
+
     ChallengeFunction.setSovler("Secant");
     ChallengeFunction.setOptions(-1,0,1.e-6,1.e-10,10000);
     ChallengeFunction.solve();
@@ -23,6 +23,13 @@ int main(){
     ChallengeFunction.setSovler("Bisection");
     ChallengeFunction.solve();
     ChallengeFunction.print();
+
+    ChallengeFunction.setSovler("Broyden");
+    ChallengeFunction.solve();
+    ChallengeFunction.print();
+
+
+
 
     return 0;
 }
