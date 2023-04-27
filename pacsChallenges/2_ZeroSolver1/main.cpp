25f1b91c5c68ce00ffcaa9c8526c39a9f1d0b8c6
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2023-04-27 10:10:25 +0200

    using setOption in the main

--- pacsChallenges/2_ZeroSolver1/main.cpp
+++ pacsChallenges/2_ZeroSolver1/main.cpp
@@ -14,6 +14,7 @@ int main(){
     FunctionTraits::FunctionType FunctionToSolve = [](double x){return 0.5-std::exp(std::numbers::pi*x);};
     FunctionRtoR ChallengeFunction(FunctionToSolve);
     ChallengeFunction.setSovler("Secant");
+    ChallengeFunction.setOptions(-1,0,1.e-6,1.e-10,10000);
     ChallengeFunction.solve();
     ChallengeFunction.print();
 
