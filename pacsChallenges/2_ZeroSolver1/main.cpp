8fade0aa85df4c69ec38c87ec186c68e5630b646
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2023-04-27 13:00:51 +0200

    .

--- pacsChallenges/2_ZeroSolver1/main.cpp
+++ pacsChallenges/2_ZeroSolver1/main.cpp
@@ -15,11 +15,12 @@ int main(){
     FunctionTraits::FunctionType FunctionToSolve = [](double x){return 0.5-std::exp(std::numbers::pi*x);};
     FunctionRtoR ChallengeFunction(FunctionToSolve);
    // ChallengeFunction.setSovler("Secant");
-    ChallengeFunction.setOptions(-1,0,1.e-6,1.e-10,10000);
+   // ChallengeFunction.setOptions(-1,0,1.e-6,1.e-10,10000);
    // ChallengeFunction.solve();
     //ChallengeFunction.print();
     std::cout<<"aaa"<< std::endl;
     ChallengeFunction.setSovler("Bisection");
+    ChallengeFunction.setOptions(-1,0,1.e-6,1.e-10,10000);
     ChallengeFunction.solve();
     ChallengeFunction.print();
 
