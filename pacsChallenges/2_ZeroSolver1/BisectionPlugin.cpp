168ce29560f21f3391a89b92077cb6f1a11004b1
Paolo Pellegrinotti - Coppolo <paolo.pellegrinotti@mail.polimi.it>
2023-04-27 11:49:29 +0200

    Implement Bisection

--- pacsChallenges/2_ZeroSolver1/BisectionPlugin.cpp
+++ pacsChallenges/2_ZeroSolver1/BisectionPlugin.cpp
@@ -1,54 +1,54 @@
 #include "BisectionPlugin.hpp"
 #include "Traits.hpp"
 #include "FactoryTraits.hpp"
+#include <iostream>
 
 namespace Solver{
+	NumericalSolutionType Bisection::solve() const{
+		point  a = opt.a; // to allow modification in the while-loop
+		point  b = opt.b;
+		double ya = fun(a);
+		double yb = fun(b);
+		double delta = b - a;
+		if(ya * yb < 0){
+			std::cerr << std::endl 
+			<< "Function must change sign at the two end values"
+			<< std::endl;
+			return std::make_pair(0, false);
+			}
+		double yc{ya};
+		double c{a};
+		while(std::abs(delta) > 2 * opt.tol_abs)
+			{
+			c = (a + b) / 2.;
+			yc = fun(c);
+			if(yc * ya < 0.0)
+				{
+				yb = yc;
+				b = c;
+				}
+			else
+				{
+				ya = yc;
+				a = c;
+				}
+			delta = b - a;
+			}
+		return std::make_pair((a + b) / 2., true);
+	}
 
-
-
-
-    
-/*!
- * Computes the zero of a scalar function with the method of the bisection
- * The returned value is far from the zero at most the given tolerance;
- *
- * @tparam Function
- * @param f The function
- * @param a First end of initial interval
- * @param b Second end of initial interval
- * @param tol Tolerance
- * @return The approximation of the zero of f
- * @pre f(a)*f(b)<0
- * @note It the interval brackets the zero convergence should be guaranteed.
- *
- */
-template <TypeTraits::ScalarFunction Function>
-double
-bisection(Function const &f, double a, double b, double tol = 1.e-5)
-{
-  double ya = f(a);
-  double yb = f(b);
-  double delta = b - a;
-  SURE_ASSERT(ya * yb < 0, "Function must change sign at the two end values");
-  double yc{ya};
-  double c{a};
-  while(std::abs(delta) > 2 * tol)
+	namespace
     {
-      c = (a + b) / 2.;
-      yc = f(c);
-      if(yc * ya < 0.0)
+        struct LoadF
         {
-          yb = yc;
-          b = c;
-        }
-      else
-        {
-          ya = yc;
-          a = c;
-        }
-      delta = b - a;
-    }
-  return (a + b) / 2.;
-}
+            LoadF(){
+                solverFactory["Bisection"] = [](FunctionTraits::FunctionType _fun) {
+                    return std::make_unique<Bisection>(_fun);
+                    };
+            }
+        };
 
-}
+        const LoadF loadf; // local variable when created loads the stuff
+    } // namespace
+
+}// namespace Solver
